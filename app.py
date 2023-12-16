e4a2ce976deca785c8bf1c5e84153807670e5095
pistaacademy <pista.academy@gmail.com>
2023-12-16 06:24:22 +0330

    Session-3

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import neattext.functions as nfx
 import base64
 import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
+import requests
 
 def make_downloadable(data,task_type):
     csvfile = data.to_csv(index=False)
@@ -23,6 +24,10 @@ def make_downloadable_df(data):
     new_filename = "extracted_data_result_{}.csv".format(timestr)
     href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!</a>'
     st.markdown(href, unsafe_allow_html=True)
+def fetch_query(query):
+    base_url = "https://www.google.com/search?q={}".format(query)
+    r = requests.get(base_url)
+    return r.text
 
 def main():
     menu = ["Home", "Single Extractor", "Bulk Extractor", "DataStorage", "About"]
@@ -30,6 +35,36 @@ def main():
 
     if choice == "Home":
         st.subheader("Search & Extractor")
+        countries_list = ["Afghanistan","Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Austrian Empire", "Azerbaijan", "Baden*", "Bahamas, The", "Bahrain", "Bangladesh", "Barbados", "Bavaria*", "Belarus", "Belgium", "Belize", "Benin (Dahomey)", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Brunswick and Lüneburg", "Bulgaria", "Burkina Faso (Upper Volta)", "Burma", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Cayman Islands, The", "Central African Republic", "Central American Federation*", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo Free State, The", "Costa Rica", "Cote d’Ivoire (Ivory Coast)", "Croatia", "Cuba", "Cyprus", "Czechia", "Czechoslovakia", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Duchy of Parma, The*", "East Germany (German Democratic Republic)", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Federal Government of Germany (1848-49)*", "Fiji", "Finland", "France", "Gabon", "Gambia, The", "Georgia", "Germany", "Ghana", "Grand Duchy of Tuscany, The*", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Hanover*", "Hanseatic Republics*", "Hawaii*", "Hesse*", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kingdom of Serbia/Yugoslavia*", "Kiribati", "Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Lew Chew (Loochoo)*", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mecklenburg-Schwerin*", "Mecklenburg-Strelitz*", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Namibia", "Nassau*", "Nauru", "Nepal", "Netherlands, The", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North German Confederation*", "North German Union*", "North Macedonia", "Norway", "Oldenburg*", "Oman", "Orange Free State*", "Pakistan", "Palau", "Panama", "Papal States*", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Piedmont-Sardinia*", "Poland", "Portugal", "Qatar", "Republic of Genoa*", "Republic of Korea (South Korea)", "Republic of the Congo", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Schaumburg-Lippe*", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands, The", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Texas*", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Two Sicilies*", "Uganda", "Ukraine", "Union of Soviet Socialist Republics*", "United Arab Emirates, The", "United Kingdom, The", "Uruguay", "Uzbekistan", "USA","UK", "Vanuatu", "Venezuela", "Vietnam", "Württemberg*", "Yemen", "Zambia", "Zimbabwe"]
+        email_extensions_list = ["gmail.com", "yahoo.com", "hotmail.com", "aol.com", "hotmail.co.uk", "hotmail.fr", "msn.com", "yahoo.fr", "wanadoo.fr", "orange.fr", "comcast.net", "yahoo.co.uk", "yahoo.com.br", "yahoo.co.in", "live.com", "rediffmail.com", "free.fr", "gmx.de", "web.de", "yandex.ru", "ymail.com", "libero.it", "outlook.com", "uol.com.br", "bol.com.br", "mail.ru", "cox.net", "hotmail.it", "sbcglobal.net", "sfr.fr", "live.fr", "verizon.net", "live.co.uk", "googlemail.com", "yahoo.es", "ig.com.br", "live.nl", "bigpond.com", "terra.com.br", "yahoo.it", "neuf.fr", "yahoo.de", "alice.it", "rocketmail.com", "att.net", "laposte.net", "facebook.com", "bellsouth.net", "yahoo.in", "hotmail.es", "charter.net", "yahoo.ca", "yahoo.com.au", "rambler.ru", "hotmail.de", "tiscali.it", "shaw.ca", "yahoo.co.jp", "sky.com", "earthlink.net", "optonline.net", "freenet.de", "t-online.de", "aliceadsl.fr", "virgilio.it", "home.nl", "qq.com", "telenet.be", "me.com", "yahoo.com.ar", "tiscali.co.uk", "yahoo.com.mx", "voila.fr", "gmx.net", "mail.com", "planet.nl", "tin.it", "live.it", "ntlworld.com", "arcor.de", "yahoo.co.id", "frontiernet.net", "hetnet.nl", "live.com.au", "yahoo.com.sg", "zonnet.nl", "club-internet.fr", "juno.com", "optusnet.com.au", "blueyonder.co.uk", "bluewin.ch", "skynet.be", "sympatico.ca", "windstream.net", "mac.com", "centurytel.net", "chello.nl", "live.ca", "aim.com", "bigpond.net.au"] 
+        country_name = st.sidebar.selectbox("Country", countries_list)
+        email_type = st.sidebar.selectbox("Email Type", email_extensions_list)
+        num_per_page = st.sidebar.number_input("Number of Results Per Page", 10, 100, step=10)
+
+        task_list = ["Emails", "URLS", "Phonenumbers"]
+        task_option = st.sidebar.multiselect("Task", task_list, default="Emails")
+        search_text = st.text_input("Paste Term Here")
+
+        generated_query = f"{search_text} + {country_name} + email@{email_type}&num={num_per_page}"
+        st.info("Generated Query: {}".format(generated_query))
+
+        if st.button("Search & Extract"):
+            if generated_query is not None:
+                text = fetch_query(generated_query)
+                
+                task_mapper = {"Emails":nfx.extract_emails(text), "URLS": nfx.extract_urls(text), "Phonenumbers": nfx.extract_phone_numbers(text)}
+
+                all_results = []
+                for task in task_option:
+                    result = task_mapper[task]
+                    all_results.append(result)
+                st.write(all_results)
+                with st.expander("Results As DataFrame"):
+                    result_df = pd.DataFrame(all_results).T
+                    result_df.columns = task_option
+                    st.dataframe(result_df)
+                    make_downloadable_df(result_df)
+
     elif choice == "Single Extractor":
         st.subheader("Extract A Single Term")
         text = st.text_area("Paste Text Here")
@@ -67,6 +102,7 @@ def main():
             result_df = pd.DataFrame(all_results).T
             result_df.columns = task_option
             st.dataframe(result_df)
+            make_downloadable_df(result_df)
     else:
         st.subheader("About")
 
